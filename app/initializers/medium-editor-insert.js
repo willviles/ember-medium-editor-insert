bd68d46348af7894c00ac760f2877e3d2bf59787
willviles <will@willviles.com>
2016-12-02 00:50:01 +0000

    Removes initializer from addon folder

--- app/initializers/medium-editor-insert.js
+++ app/initializers/medium-editor-insert.js
@@ -1,1 +1,29 @@
-export default 'ember-medium-editor-insert/initializers/medium-editor-insert';
+import Ember from 'ember';
+import mediumEditor from 'ember-cli-medium-editor/components/medium-content-editable';
+
+const { get, observer, merge } = Ember;
+
+export function initialize() {
+
+  mediumEditor.reopen({
+
+    editorInitialized: observer('mediumEditor', function() {
+
+      this._super(...arguments);
+
+      const opts = merge(
+        { editor: get(this, 'mediumEditor')},
+        get(this, 'insertOptions')
+      );
+
+      this.$().mediumInsert(opts);
+
+    })
+
+  });
+}
+
+export default {
+  name: 'medium-editor-insert',
+  initialize: initialize
+};
